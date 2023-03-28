7ec952ee55ef96570335aecc06316e626be6d537
clay-hacker-sys <microsoft.corporation.net@gmail.com>
2023-03-28 15:20:47 +0300

    First commit VSC

---
+++ calc.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+int main() {
+  float num1, num2;
+  char op;
+
+  std::cout << "Введите первое число: ";
+  std::cin >> num1;
+  std::cout << "Введите оператор (+, -, *, /): ";
+  std::cin >> op;
+  std::cout << "Введите второе число: ";
+  std::cin >> num2;
+
+  float result;
+
+  switch(op) {
+    case '+':
+      result = num1 + num2;
+      break;
+    case '-':
+      result = num1 - num2;
+      break;
+    case '*':
+      result = num1 * num2;
+      break;
+    case '/':
+      if (num2 == 0) {
+        std::cout << "Ошибка: деление на ноль" << std::endl;
+        return 1;
+      }
+      result = num1 / num2;
+      break;
+    default:
+      std::cout << "Ошибка: недопустимый оператор" << std::endl;
+      return 1;
+  }
+
+  std::cout << "Результат: " << result << std::endl;
+
+  return 0;
+}
